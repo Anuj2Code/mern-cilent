9040a9725728fb8c203b5f1f3a73d32e5efb5e49
Anuj pal <148964497+Anuj2Code@users.noreply.github.com>
2024-03-12 14:24:50 +0530

    Update Sidebar.js

--- commerce/src/Componets/Dashboard.js/Sidebar.js
+++ commerce/src/Componets/Dashboard.js/Sidebar.js
@@ -1,16 +1,7 @@
 import React from "react";
 import "./Sidebar.css";
-// import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
-import { TreeView, TreeItem } from "@material-ui/lab";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import PostAddIcon from "@material-ui/icons/PostAdd";
-import AddIcon from "@material-ui/icons/Add";
-import ImportExportIcon from "@material-ui/icons/ImportExport";
-import ListAltIcon from "@material-ui/icons/ListAlt";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import PeopleIcon from "@material-ui/icons/People";
-import RateReviewIcon from "@material-ui/icons/RateReview";
+
 
 const Sidebar = () => {
   return (
@@ -20,34 +11,22 @@ const Sidebar = () => {
       </Link>
       <Link to="/admin/dashboard">
         <p>
-          <DashboardIcon /> Dashboard
+         <h1>Dashboard</h1>
         </p>
       </Link>
-      <Link>
-        <TreeView
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpandIcon={<ImportExportIcon />}
-        >
-          <TreeItem nodeId="1" label="Products">
-            <Link to="/admin/products">
-              <TreeItem nodeId="2" label="All" icon={<PostAddIcon />} />
-            </Link>
-
+      <Link> 
             <Link to="/admin/product">
-              <TreeItem nodeId="3" label="Create" icon={<AddIcon />} />
+             <h1>Add Pro</h1>
             </Link>
-          </TreeItem>
-        </TreeView>
       </Link>
       <Link to="/admin/orders">
         <p>
-          <ListAltIcon />
           Orders
         </p>
       </Link>
       <Link to="/admin/users">
         <p>
-          <PeopleIcon /> Users
+         Users
         </p>
       </Link>
     </div>
